211098ece95b46615b18de9c32a0f5925108514f
n-ce <abrocks1120@gmail.com>
2021-09-26 13:18:21 +0530

    new interface, dark mode support, externalized styles for modules

--- script.js
+++ script.js
@@ -2,19 +2,26 @@
 
 var nav = document.getElementsByTagName('h4');
 
-function fetchHtml(a,b) {
-  fetch("https://raw.githubusercontent.com/n-ce/SPACE-Modules/main/"+a+".html")
+function fetchHtml(a, b) {
+  fetch("https://raw.githubusercontent.com/n-ce/SPACE-Modules/main/" + a + ".html")
     .then((response) => {
       return response.text();
     })
     .then((html) => {
       document.getElementById('root').innerHTML = html;
       document.querySelector('header').innerText = a;
-      for ( i = 0; i < 3; i++){
-        nav[i].style.color="#fff7";
-        nav[i].style.animationName=undefined;
-      }
-      nav[b].style.color="#fff";
-      nav[b].style.animationName="bubble";
     });
+}
+
+if (window.localStorage.getItem('theme') == "dark") {
+  dark();
+}
+
+function dark() {
+  document.body.classList.toggle("bodyDark");
+  for (var i = 0; i < 4; i++) {
+    document.querySelectorAll('.button')[i].classList.toggle("buttonDark");
+  }
+  window.localStorage.setItem('theme', "dark");
+
 }
