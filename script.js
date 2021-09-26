368b863651d220de0454c68ffd119d312640c762
n-ce <abrocks1120@gmail.com>
2021-09-26 15:42:12 +0530

    50% inconsistency fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var nav = document.getElementsByTagName('h4');
 
-function fetchHtml(a, b) {
+function fetchHtml(a) {
   fetch("https://raw.githubusercontent.com/n-ce/SPACE-Modules/main/" + a + ".html")
     .then((response) => {
       return response.text();
