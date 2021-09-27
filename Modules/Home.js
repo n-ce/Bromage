2dae131d79af962f3cb357597454ba474e5cd572
JanviRaina <janvijeevita@gmail.com>
2021-09-28 03:12:50 +0530

    added javascript file

--- Modules/Home.js
+++ Modules/Home.js
@@ -1,5 +1,44 @@
-fetch('https://api.quotable.io/random')
-  .then(response => response.json())
-  .then(data => {
-    document.querySelector('#quotes').innerText="\""+data.content+"\"\n\n"+data.author;
-  })
+$(document).ready(function()
+{
+$("#form-submit").submit(
+    function(event)
+    {
+        fetch_data(event);
+    }
+)
+});
+
+function fetch_data(event)
+{
+    var request;
+    event.preventDefault();
+    request=$.ajax
+    (
+        {
+            url:'https://api.openweathermap.org/data/2.5/weather',
+            type : "GET",
+            data :
+            {
+                q: $("#city").val(),
+                /*Enter here user key provided when you register on the website
+                https://home.openweathermap.org/api_keys*/
+                appid:'user_api_key',
+                units:'metric'
+            }
+        }
+    );
+    request.done(function(response)
+    {
+        format_data(response);
+    });
+}
+
+function format_data(jsonObject)
+{
+    var city_name=jsonObject.name;
+    var city_weather =jsonObject.weather[0].main;
+    var city_temp=jsonObject.main.temp;
+    $("#city-name").text("Displaying result for the city "+ city_name);
+    $("#city-weather").text("Current Weather is "+city_weather);
+    $("#city-temp").text("Current Temperature is "+ city_temp +" Celsius");
+}
