956fab87eb416b34bd1fe86421df42a4810708b2
Animesh Nath <69345507+n-ce@users.noreply.github.com>
2021-09-28 12:32:57 +0530

    Update Home.js

--- Modules/Home.js
+++ Modules/Home.js
@@ -3,8 +3,3 @@ fetch('https://api.quotable.io/random')
   .then(data => {
     document.getElementById('quotes').innerText="\""+data.content+"\"\n\n"+data.author;
   })
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
-  .then(data => {
-     document.getElementById('dogs').src = data.message;
-  })
