84f2f27a0aeaf62f08b10b40cc457f780f0af0c5
n-ce <abrocks1120@gmail.com>
2021-09-28 19:58:52 +0530

    Fixed weather API null element bug

--- Modules/Home.js
+++ Modules/Home.js
@@ -1,31 +1,27 @@
 fetch('https://api.quotable.io/random')
-.then(response => response.json())
-.then(data => {
-  document.getElementById('quotes').innerText="\""+data.content+"\"\n\n"+data.author;
-})
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('quotes').innerText = "\"" + data.content + "\"\n\n" + data.author;
+  })
 
-let temperature = document.querySelector(".temp");
-let weather = document.querySelector(".weather");
-let loc = document.querySelector(".location");
 
-let longitude,latitude;
+
+let longitude, latitude;
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => 
-    {
-        console.log(position);
-        longitude = position.coords.longitude;
-        latitude = position.coords.latitude;
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&` +
-            `lon=${longitude}&appid=6d055e39ee237af35ca066f35474e9df`)
-            .then((response) => {
-            return response.json();
-            })
-            .then((data) => {
-            console.log(data);
-            temperature.textContent =
-                Math.floor(data.main.temp - 273) + "°C";
-            weather.textContent = data.weather[0].description;
-            loc.textContent = data.name + "," + data.sys.country;
-            });
-	});
+  navigator.geolocation.getCurrentPosition((position) =>
+  {
+    console.log(position);
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&` + `lon=${longitude}&appid=6d055e39ee237af35ca066f35474e9df`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        document.querySelector("#temp").innerText = Math.floor(data.main.temp - 273) + "°C";
+        document.querySelector("#weather").innerText = data.weather[0].description;
+        document.querySelector("#location").innerText = data.name + "," + data.sys.country;
+      });
+  });
 }
