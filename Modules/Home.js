984ef9b37e3908eac1ce624fa26b79ea7bb35278
Animesh Nath <69345507+n-ce@users.noreply.github.com>
2021-09-29 14:45:54 +0530

    Update Home.js

--- Modules/Home.js
+++ Modules/Home.js
@@ -20,5 +20,5 @@ if (navigator.geolocation) {
 fetch('https://api.quotable.io/random')
   .then(res => res.json())
   .then(data => {
-    document.getElementById('quotes').innerText = "\"" + data.content + "\"\n\n" + data.author;
+    document.getElementById('quotes').innerText =  `"${data.content}"\n\n${data.author}`;
   })
