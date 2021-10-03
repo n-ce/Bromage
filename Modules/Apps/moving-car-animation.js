5bfd6a1281b3e789433eff4e16b41ef39b190783
Subhadeep Das <63661386+Raven1233@users.noreply.github.com>
2021-10-03 21:04:37 +0530

    Update moving-car-animation.js

--- Modules/Apps/moving-car-animation.js
+++ Modules/Apps/moving-car-animation.js
@@ -1,30 +1,26 @@
-$(document).ready(function(){
-    $surface=$('.surface');
-    $car=$('.car');
-    $img=$('.car img');
-    $song=$('#mySong');
+document.addEventListener("DOMContentLoaded", function() {
     let flag = true;
    
     const cars=['https://github.com/Raven1233/Moving-car-animation/blob/master/Img_05.png?raw=true', 'https://github.com/Raven1233/Moving-car-animation/blob/master/Img_06.png?raw=true'];
-    
-    $(document).on('keypress',function(f){
+
+    document.addEventListener('keypress', (f) => {
         if(f.which == 13){
-            $($surface).toggleClass('movement');
-            $($car).toggleClass('carmove');
+            document.getElementById("surface").classList.toggle("movement");
+            document.getElementById("car").classList.toggle("carmove");
         }
-    })
-    $(document).on('keypress',function(f){
+    });
+    document.addEventListener('keypress', (f) => {
         if(f.which == 119){
             if(flag){
                 flag=false;
-                $img.attr('src',cars[0]);
+                document.getElementById("car_image").src = cars[0];
             }
             else{
                 flag=true;
-                $img.attr('src',cars[1]);
+                document.getElementById("car_image").src = cars[1];
             }
         }
-    })
+    });
     document.addEventListener('keydown', function(e) {
         if (e.keyCode == 49) {
           document.getElementById('audio').play();
